25aadd905119aea76a11641a6b32efd94beac175
SHREYA MITTAL <mittalshreya93@gmail.com>
2017-03-23 23:17:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,26 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by SHREYA on 3/23/2017.
+ */
+public final class PetContract {
+
+    private PetContract() {
+    }
+
+    public static final class PetEntry implements BaseColumns{
+
+        public final static String _ID=BaseColumns._ID;
+        public final static String TABLE_NAME="pets";
+        public final static String COLUMN_PET_NAME="name";
+        public final static String COLUMN_PET_BREED="breed";
+        public final static String COLUMN_PET_GENDER="gender";
+        public final static String COLUMN_PET_WEIGHT="weight";
+
+        public final static int GENDER_UNKNOWN=0;
+        public final static int GENDER_MALE=1;
+        public final static int GENDER_FEMALE=2;
+    }
+}
