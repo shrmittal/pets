b699f33e37c0551f052463591c1a77aa2ecb0e23
SHREYA MITTAL <mittalshreya93@gmail.com>
2017-03-25 02:20:34 +0530

    prevent crash with blank editor

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -138,7 +138,12 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         String nameString = mNameEditText.getText().toString().trim();
         String breedString = mBreedEditText.getText().toString().trim();
         String weightString = mWeightEditText.getText().toString().trim();
-        int weight = Integer.parseInt(weightString);
+
+       // int weight = Integer.parseInt(weightString);
+
+        if(mCurrentPetUri==null && TextUtils.isEmpty(nameString)&&TextUtils.isEmpty(breedString)
+                &&TextUtils.isEmpty(weightString)&&mGender==PetEntry.GENDER_UNKNOWN)
+            return;
 
 
         //we don't require the following now.
@@ -151,6 +156,12 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         values.put(PetEntry.COLUMN_PET_NAME, nameString);
         values.put(PetEntry.COLUMN_PET_BREED, breedString);
         values.put(PetEntry.COLUMN_PET_GENDER, mGender);
+
+        int weight =0;
+        if(!TextUtils.isEmpty(weightString))
+            weight=Integer.parseInt(weightString);
+
+
         values.put(PetEntry.COLUMN_PET_WEIGHT, weight);
 
         if (mCurrentPetUri == null) {
